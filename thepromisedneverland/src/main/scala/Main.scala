ac15418080d99daa9086b9fbaf6c8f078918ecc7
git-akihakune <akihakune@protonmail.ch>
2023-11-24 22:44:06 +0000

    Refactor and add interface methods

--- thepromisedneverland/src/main/scala/Main.scala
+++ thepromisedneverland/src/main/scala/Main.scala
@@ -10,8 +10,9 @@ object AdventureTextUI extends App:
   this.run()
 
   private def run() =
-    ui.slowPrint(this.game.welcomeMessage)
-    ui.slowPrint(this.game.warningMessage)
+    ui.artisticPrint(this.game.welcomeMessage, "cyan")
+    ui.artisticPrint(this.game.warningMessage, "red")
+    ui.resetTextColor
     while !this.game.isOver do
       this.printAreaInfo()
       this.playTurn()
@@ -22,18 +23,18 @@ object AdventureTextUI extends App:
     val time = this.game.timeReport
     val fullDescription = area.name + ", " + time
     
-    ui.slowPrint(area.areaArt, Option(500))
+    ui.artisticPrint(area.areaArt, "white", Option(500))
 
-    ui.slowPrint("\n\n" + fullDescription)
-    ui.slowPrint("-" * fullDescription.length)
-    ui.slowPrint(area.fullDescription + "\n")
+    ui.artisticPrint("\n\n" + fullDescription, "green")
+    ui.artisticPrint("-" * fullDescription.length, "yellow")
+    ui.artisticPrint(area.fullDescription + "\n")
 
   private def playTurn() =
-    println()
-    val command = readLine("What to do next?\n> ")
+    ui.artisticPrint("\nWhat to do next?\n> ", "magenta", noNewline=true)
+    val command = readLine()
     val turnReport = this.game.playTurn(command)
     if turnReport.nonEmpty then
-      ui.slowPrint(turnReport)
+      ui.artisticPrint(turnReport)
 
 end AdventureTextUI
 
--- thepromisedneverland/src/main/scala/adventure/Interface.scala
+++ thepromisedneverland/src/main/scala/adventure/Interface.scala
@@ -1,7 +1,47 @@
 package adventure
 
 class Interface:
-    def slowPrint(line: String, waitTime: Option[Int] = None) =
+    def resetTextColor = 
+        print(Console.RESET)
+
+    def colorText(content: String, color: String): String =
+        val ansiColor = color match {
+            case "blue" => Console.BLUE
+            case "red" => Console.RED
+            case "green" => Console.GREEN
+            case "cyan" => Console.CYAN
+            case "yellow" => Console.YELLOW
+            case "magenta" => Console.MAGENTA
+            case "white" => Console.WHITE
+            case "black" => Console.BLACK
+        }
+        ansiColor + content 
+
+    def artisticPrint(content: String, color: String = "white", waitTime: Option[Int] = None, noNewline: Boolean = false): Unit =
         val sleepTime = waitTime.getOrElse(200)
         Thread.sleep(sleepTime)
-        println(line)
+        if noNewline then
+            print(this.colorText(content, color))
+        else
+            println(this.colorText(content, color))
+        this.resetTextColor
+
+    def setBackgroundColor(color: String): Unit =
+        val ansiColor = color match {
+            case "black" => Console.BLACK_B
+            case "red" => Console.RED_B
+            case "green" => Console.GREEN_B
+            case "yellow" => Console.YELLOW_B
+            case "blue" => Console.BLUE_B
+            case "magenta" => Console.MAGENTA_B
+            case "cyan" => Console.CYAN_B
+            case "white" => Console.WHITE_B
+        }
+        print(ansiColor)
+    
+    // untested, use with caution
+    def invisibleText(content: String): String = Console.INVISIBLE + content
+    def reversedText(content: String): String = Console.REVERSED + content
+    def underlinedText(content: String): String = Console.UNDERLINED + content
+    def boldText(content: String): String = Console.BOLD + content
+    def blinkText(content: String): String = Console.BLINK + content
